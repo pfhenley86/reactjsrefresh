249903a8f58d084058e4509415b98e7dc5426c79
Ford Henley <info@fordswebhelp.com>
2025-03-27 09:52:17 -0500

    started task app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
 import "./styles.css";
 
 export default function  App(){
-  return <form className="new-item-form">
+  const [newItem, setNewItem ] = useState("")
+  const [todos, setTodos] = useState([])
 
-  </form>
+  function handleSubmit(e){
+    e.preventDefault()
+  }
+
+  return (
+    <> 
+    <form onSubmit={handleSubmit} className="new-item-form">
+    <div className="form-row">
+      <label htmlFor="item">New Item</label>
+      <input value={newItem} onChange={e => setNewItem(e.target.value)} 
+        type="text" 
+        id="item" 
+        name="item"
+        />  
+    </div>
+    <button className="btn">Add</button>
+    </form>
+    <h1 className="header">Todo List</h1>
+    <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />Item 1
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />Item 2
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+    </ul>
+  </>
+  )
 }
