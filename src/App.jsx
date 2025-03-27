2d4d4bb2f98970d6a8aaeda9837d17ba51199097
Ford Henley <info@fordswebhelp.com>
2025-03-27 10:29:04 -0500

    finished todo setup

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,34 @@ export default function  App(){
 
   function handleSubmit(e){
     e.preventDefault()
+
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos, 
+        { id: crypto.randomUUID(), title: newItem, completed: false}
+      ]
+    })
+
+    setNewItem("")
+  }
+
+  function toggleTodo(id, completed){
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = completed
+          return { ...todo, completed }
+        }
+
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id){
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id)
+    })
   }
 
   return (
@@ -24,18 +52,19 @@ export default function  App(){
     </form>
     <h1 className="header">Todo List</h1>
     <ul className="list">
-      <li>
-        <label>
-          <input type="checkbox" />Item 1
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
-      <li>
-        <label>
-          <input type="checkbox" />Item 2
-        </label>
-        <button className="btn btn-danger">Delete</button>
-      </li>
+      {todos.length === 0 && "No Todos"}
+      {todos.map(todo => {
+        return (
+          <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed}
+              onChange={e => toggleTodo(todo.id, e.target.checked)} />
+              {todo.title}
+            </label>
+            <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
+          </li>
+        )
+      })}
     </ul>
   </>
   )
